bd097be1eb4083ebdbec82cc89cbef6abae953f2
Akashpandey1507 <Akashpandey1507>
2023-10-22 15:04:15 +0000

    updates file

--- notebook/Data_Storage.py
+++ notebook/Data_Storage.py
@@ -1,2 +1,16 @@
 # Databricks notebook source
+# MAGIC %run "/Repos/akashpandey15071996@outlook.com/end-to-end-supply-chain-project-with-azure-databricks-and-pipeline/notebook/Data_Preparation"
 
+# COMMAND ----------
+
+display(dbutils.fs.ls("wasbs://" + blob_container + "@" + storage_account_name + ".blob.core.windows.net/"))
+
+# COMMAND ----------
+
+# Specify the write mode as "overwrite"
+df2.write.mode("overwrite").csv("wasbs://supply-chain-datasets@project99110.blob.core.windows.net/cleaneddatasets/Tokendatasets/")
+
+# COMMAND ----------
+
+# Specify the write mode as "overwrite"
+df1.write.mode("overwrite").csv("wasbs://supply-chain-datasets@project99110.blob.core.windows.net/cleaneddatasets/SupplyChainDataset/")
--- notebook/Exploratory_Data_Analysis.py
+++ notebook/Exploratory_Data_Analysis.py
@@ -1,2 +1,6 @@
 # Databricks notebook source
+# MAGIC %run "/Repos/akashpandey15071996@outlook.com/end-to-end-supply-chain-project-with-azure-databricks-and-pipeline/notebook/Data_Preparation"
+
+# COMMAND ----------
+
 
