5827d69d632fa0234f41b282841afa88d0d19750
Akashpandey1507 <Akashpandey1507>
2023-10-22 12:33:58 +0000

    update

--- notebook/Data_Ingestion.py
+++ notebook/Data_Ingestion.py
@@ -1,2 +1,17 @@
 # Databricks notebook source
+# Getting all details from Azure
+storage_account_name = 'enter the storage name'
+storage_account_access_key = 'Enter the keys'
+blob_container = 'enter the container name'
 
+# COMMAND ----------
+
+spark.conf.set('fs.azure.account.key.' + storage_account_name + '.blob.core.windows.net', storage_account_access_key)
+
+# COMMAND ----------
+
+filePath = "wasbs://" + blob_container + "@" + storage_account_name + ".blob.core.windows.net/"
+
+# COMMAND ----------
+
+display(dbutils.fs.ls(filePath))
