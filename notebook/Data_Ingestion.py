fdaa75629ee49581fc37ba2e87e4b9f5c3b3cffd
Akashpandey1507 <Akashpandey1507>
2023-10-24 08:20:31 +0000

    updates data_ingetion .py

--- notebook/Data_Ingestion.py
+++ notebook/Data_Ingestion.py
@@ -1,8 +1,14 @@
 # Databricks notebook source
+#create the scope
+secret_value = dbutils.secrets.get(scope="AkashScope", key="adlskey")
+secret_storage_name = dbutils.secrets.get(scope="AkashScope", key="storagename")
+
+# COMMAND ----------
+
 # Getting all details from Azure
-storage_account_name = 'enter the storage name'
-storage_account_access_key = 'Enter the keys'
-blob_container = 'enter the container name'
+storage_account_name = secret_storage_name
+storage_account_access_key = secret_value
+blob_container = 'supply-chain-datasets'
 
 # COMMAND ----------
 
@@ -10,8 +16,32 @@ spark.conf.set('fs.azure.account.key.' + storage_account_name + '.blob.core.wind
 
 # COMMAND ----------
 
-filePath = "wasbs://" + blob_container + "@" + storage_account_name + ".blob.core.windows.net/"
+filePath = "wasbs://" + blob_container + "@" + storage_account_name + ".blob.core.windows.net/rawdatasets/"
 
 # COMMAND ----------
 
 display(dbutils.fs.ls(filePath))
+
+# COMMAND ----------
+
+maindata = spark.read.csv(
+                            "wasbs://supply-chain-datasets@project99110.blob.core.windows.net/rawdatasets/DataCoSupplyChainDataset.csv",
+                            header=True,
+                            inferSchema=True
+)
+
+# COMMAND ----------
+
+metadata = spark.read.csv(
+                            "wasbs://supply-chain-datasets@project99110.blob.core.windows.net/rawdatasets/DescriptionDataCoSupplyChain.csv",
+                            header=True,
+                            inferSchema=True
+)
+
+# COMMAND ----------
+
+tockendatasets = spark.read.csv(
+                            "wasbs://supply-chain-datasets@project99110.blob.core.windows.net/rawdatasets/tokenized_access_logs.csv",
+                            header=True,
+                            inferSchema=True
+)
